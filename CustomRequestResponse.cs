c6b9f6f43061cc039b8aeeb8373b7060ed6219a3
SalmonLoki <kseniiakrovlya@gmail.com>
2019-05-06 14:50:04 +0300

    HttpWebRequest -> custom wrapping IHttpWebRequest HttpWebResponse -> custom wrapping IHttpWebResponse

---
+++ CustomRequestResponse.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace CustomSearch
+{
+    public static class CustomRequestResponse
+    {
+        public interface IHttpWebRequest
+        {
+            IHttpWebResponse GetResponse();
+        }
+
+        public interface IHttpWebResponse : IDisposable
+        {
+            Stream GetResponseStream();
+        }
+
+
+        public class WrapHttpWebRequest : IHttpWebRequest
+        {
+            private readonly HttpWebRequest _request;
+
+            public WrapHttpWebRequest(HttpWebRequest request)
+            {
+                _request = request;
+            }
+
+            public virtual IHttpWebResponse GetResponse()
+            {
+                return new WrapHttpWebResponse((HttpWebResponse)_request.GetResponse());
+            }
+        }
+
+        public class WrapHttpWebResponse : IHttpWebResponse
+        {
+            private WebResponse _response;
+
+            public WrapHttpWebResponse(HttpWebResponse response)
+            {
+                _response = response;
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            private void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    if (_response != null)
+                    {
+                        _response.Dispose();
+                        _response = null;
+                    }
+                }
+            }
+
+            public virtual Stream GetResponseStream()
+            {
+                return _response.GetResponseStream();
+            }
+        }
+    }
+}
--- YandexWebSearcher.cs
+++ YandexWebSearcher.cs
@@ -4,34 +4,44 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Linq;
 using System.Configuration;
+using System.IO;
+using static CustomSearch.CustomRequestResponse;
 
 namespace CustomSearch
 {
-    class YandexWebSearcher : IWebSearcher
+    public class YandexWebSearcher : IWebSearcher
     {
         private string subscriptionKey = ConfigurationManager.AppSettings["yandexSubscriptionKey"];
         private string yandexLogin = ConfigurationManager.AppSettings["yandexUser"];
         private string template = @"https://yandex.com/search/xml?query={0}&l10n=en&user={1}&key={2}&count={3}";
 
+        IHttpWebRequest requestI;
+
+        public YandexWebSearcher(IHttpWebRequest requestI = null)
+        {
+            this.requestI = requestI;
+        }
+
         public SearchResult[] Search(string keyword, int resultCount)
         {
             string completeUrl = string.Format(template, keyword, yandexLogin, subscriptionKey, resultCount);
-            
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(completeUrl);
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+
+            IHttpWebRequest newRequest = requestI ?? new WrapHttpWebRequest((HttpWebRequest)WebRequest.Create(completeUrl));
+            using (IHttpWebResponse response = newRequest.GetResponse() )
             {
-                return xmlList(response);
+                Stream stream = response.GetResponseStream();
+                return XmlList(stream);
             }           
         }
 
-        private SearchResult[] xmlList(HttpWebResponse response)
+        private SearchResult[] XmlList(Stream stream)
         {
-            XDocument xDoc = XDocument.Load(XmlReader.Create(response.GetResponseStream()));
+            XDocument xDoc = XDocument.Load(XmlReader.Create(stream));
 
             IEnumerable<XElement> query = xDoc.Descendants().Descendants("response").Descendants("results").Descendants("grouping")
                                           .Descendants("group").Descendants("doc");
 
-            return query.Select(u => new SearchResult { Link = u.Element("url").Value, Name = u.Element("title").Value }).ToArray();
+            return query.Select(u => new SearchResult { Link = u.Element("url").Value.Trim(), Name = u.Element("title").Value.Trim() }).ToArray();
         }
     }
 }
